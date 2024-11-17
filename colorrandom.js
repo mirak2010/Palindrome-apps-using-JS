c225f374df72fb29c9af2a3105f7165d28c4de5b
mirak2010 <mirakhmad0101@yahoo.com>
2024-11-17 10:51:47 +0500

    #

--- colorrandom.js
+++ colorrandom.js
@@ -1,3 +1,10 @@
-function myfunction(){
+function changeColor(){
+   var r=Math.round(Math.random()*255);
    
+   var g=Math.round(Math.random()*255);
+   var b=Math.round(Math.random()*255);
+   var result="rgb("+r+","+g+","+b+")"
+   var d="rgb("+1+","+4+","+3+")";
+   
+   document.body.style.backgroundColor=result;
 }
